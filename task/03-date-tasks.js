86d023b7dd31961b88ae98fce3c18757ee85cf50
KostyaPolhovskiy <Kosty57115711@mail.ru>
2018-05-04 01:01:29 +0300

    100%

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-    throw new Error('Not implemented');
+    return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-    throw new Error('Not implemented');
+    return new Date(value);
 }
 
 
@@ -56,7 +56,9 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-    throw new Error('Not implemented');
+    let year = date.getFullYear(date);
+    if (year % 100 !== 0) return year % 4 == 0;
+    else return year % 400 == 0;
 }
 
 
@@ -76,7 +78,7 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-    throw new Error('Not implemented');
+    return new Date(endDate - startDate).toISOString().slice(11, 23);
 }
 
 
@@ -94,7 +96,14 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+    var hours = date.getUTCHours();
+    var mins = date.getUTCMinutes();
+    if (hours > 12)
+        hours -= 12;
+    var angle = Math.abs(0.5 * (60 * hours - 11 * mins));
+    if (angle > 180)
+        angle = 360 - angle;
+    return angle * (Math.PI / 180);
 }
 
 
